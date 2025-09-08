044c110d2608aef3c766166c6dd0fb1372f61052
Tagen Burke <tagen.s.burke-1@ou.edu>
2025-09-08 12:10:16 -0500

    HW1a_Sales Tagen Burke Complete

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name: Tagen Burke
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -13,8 +13,68 @@ namespace HW1a_Sales
 {
     internal class Program
     {
+        /*
+         * (Order)
+         * Question (string)
+         * String ticket amount + Enter float(double)
+         * String price of each ticker+ Enter float(double)
+         * string Your subtotal + Calulate (ticket * price)
+         * string tax + Calulate (subtotal * 0.085)
+         * string total + Calulate (subtotal + tax)
+         */
+
         static void Main(string[] args)
         {
+        string question = "what is the sport name of the ticket you are purchasing? \t (Football or Soccer)";
+            const string Football = "football";
+            const string Soccer = "soccer";
+            string TicketName;
+
+        string ticketAmount = "How many tickets are you purchasing?";
+            double tickets;
+
+        string priceOfEachTicket = "Price of each ticket:";
+            double price;
+
+        string yourSubtotal = "\v\tYour subtotal for your bill is: ";
+            double subtotal;
+
+        string taxString = "\tYour salestax for your bill is: ";
+            double tax;
+
+        string totalString = "\tYour total for your bill is: ";
+            double total;
+
+            Console.WriteLine(question);
+            TicketName = Console.ReadLine();
+            switch (TicketName.ToLower())///Found this on page 71 of the textbook
+            {
+                case Football:
+                    price = 49.99f;
+                    break;
+                case Soccer:
+                    price = 39.99f;
+                    break;
+                default:
+                    price = 49.99f;
+                    break;
+            }
+     
+            Console.WriteLine(ticketAmount);
+            tickets = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(priceOfEachTicket + price.ToString("N2"));
+
+            subtotal = tickets * price;
+            Console.WriteLine(yourSubtotal + subtotal.ToString("C"));
+
+            tax = subtotal * 0.085;
+            Console.WriteLine(taxString + tax.ToString("C"));
+
+            total = subtotal + tax;
+            Console.WriteLine(totalString + total.ToString("C"));
+            Console.ReadLine();
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -11,10 +11,93 @@ using System.Threading.Tasks;
 
 namespace HW1b_Grade
 {
+    /*
+     * Sylabuss weights
+     * Assignments 20%
+     * Exam 1 15%
+     * Exam 2 25%
+     * Exam 3 25%
+     * Participation 15%
+     * Quizzes 0%
+     * 
+     * Ouptput
+     * String First Name
+     * String Last Name
+     * String Student ID
+     * String percentage for Assignments 
+     * String percentage for Participation
+     * String percentage for Quizzes
+     * String percentage for Exam 1
+     * String percentage for Exam 2
+     * String percentage for Exam 3
+     * Final Concation First Name + Last Name + Student ID + Final Grade
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Weights
+            double AssignmentsWeight = 0.20;
+            double Exam1Weight = 0.15;
+            double Exam2Weight = 0.25;
+            double Exam3Weight = 0.25;
+            double ParticipationWeight = 0.15;
+            double QuizzesWeight = 0;
+
+
+
+            //Strings
+            string firstNameString = "Enter your first name:";
+            string FirstName;
+
+            string lastNameString = "Enter your last name:";
+            string LastName;
+
+            string studentIDString = "Enter your student ID:";
+            string StudentID;
+
+            string assignmentsString = "Enter your percentage for Assignments:";
+            string participationString = "Enter your percentage for Participation:";
+            string quizzesString = "Enter your percentage for Quizzes:";
+            string exam1String = "Enter your percentage for Exam 1:";
+            string exam2String = "Enter your percentage for Exam 2:";
+            string exam3String = "Enter your percentage for Exam 3:";
+            string finalGradeString = "Your final grade is: ";
+
+            Console.WriteLine(firstNameString);
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine(lastNameString);
+            LastName = Console.ReadLine();
+
+            Console.WriteLine(studentIDString);
+            StudentID = Console.ReadLine();
+
+            Console.WriteLine(assignmentsString);
+            double assignments = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(participationString);
+            double participation = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(quizzesString);
+            double quizzes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(exam1String);
+            double exam1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(exam2String);
+            double exam2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(exam3String);
+            double exam3 = Convert.ToDouble(Console.ReadLine());
+
+            double FinaleGrade = (assignments * AssignmentsWeight) + (participation * ParticipationWeight) + (quizzes * QuizzesWeight) + (exam1 * Exam1Weight) + (exam2 * Exam2Weight) + (exam3 * Exam3Weight);
+
+            Console.WriteLine(finalGradeString);
+            Console.WriteLine(FinaleGrade);
+            
+            Console.Write(FirstName + " " + LastName + " " + "(" + StudentID + ")" +", " + "your final grade is" + " " + $"{FinaleGrade:F2}%");
+        
         }
     }
 }
